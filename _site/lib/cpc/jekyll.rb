6d3e4c09a350a83865e59dc2920a8274005f42c5
alexander <agarber@profectusgroup.com>
2019-11-25 21:25:11 +1100

    jekyll working

---
+++ _site/lib/cpc/jekyll.rb
@@ -0,0 +1,12 @@
+require "cpc/jekyll/version"
+
+module Cpc
+  module Jekyll
+    class Error < StandardError; end
+
+    def hello_jekyll
+      "hello_jekyll"
+    end
+    # Your code goes here...
+  end
+end
---
+++ _site/spec/cpc/codewars/drying_potatoes_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe Cpc::Codewars::DryingPotatoes do
+  include Cpc::Codewars::DryingPotatoes
+#
+  it 'should return the dried weight of 82kg', offline: true do
+    expect(potatoes(82, 127, 80)).to eq(114)
+  end
+
+  it "should return the dried weight of 93kg", offline: true do
+    expect(potatoes(93, 129, 91)).to eq(100)
+  end
+end
