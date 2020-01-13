7c5c50dd2f62fec33c575915c0caf53b0ebd0ce3
alexandergarber <agarber@profectusgroup.com>
2020-01-13 13:11:39 +1100

    project rename

--- bin/setup.rb
+++ bin/setup.rb
@@ -2,19 +2,21 @@ require 'pry'
 require 'fileutils'
 
 project_details_hsh = JSON.parse(File.read('bin/project_details.json'))
-module_str = project_details_hsh["module"]
-spec_str = project_details_hsh["spec"]
+old_module_str = project_details_hsh["module"]
+old_spec_str = project_details_hsh["spec"]
+new_module_str = 'Foobar'
+new_spec_str = 'foobar'
 
 def project_files
-  grep_ary = `grep -rnw -e "#{project_details["module"]}\\|#{project_details["spec"]}"`.split("\n")
+  grep_ary = `grep -rnw -e "#{old_module_str}\\|#{old_spec_str}"`.split("\n")
   grep_ary.map {|x| x.split(':').first unless x.match?(".git")}.compact.uniq
 end
 
-def rename_module_and_spec(path_ary, module_str, spec_str)
+def rename_module_and_spec(path_ary)
   path_ary = project_files
   path_ary.each do |path|
     r = File.read(path)
-    w = r.gsub(project_details["module"], module_str).gsub(project_details["spec"], spec_str)
+    w = r.gsub(old_module_str, new_module_str).gsub(old_spec_str, new_spec_str)
 
     f = File.open(path, 'w')
     f.write(w)
@@ -25,9 +27,9 @@ end
 
 def rename_files(path_ary)
   path_ary.each do |path|
-    if File.basename.match?(project_details["spec"])
+    if File.basename.match?(old_spec_str)
       dirname = File.dirname(f)
-      new_basename = File.basename.sub(project_details["spec"], spec_str)
+      new_basename = File.basename.sub(old_spec_str, new_spec_str)
       new_path = [dirname, new_basename].join('/')
       puts "New path: #{new_path}"
       File.rename(path, dirname, new_path)
@@ -42,6 +44,5 @@ def rename_project
   path_ary_02 = project_files
   rename_module_and_spec(path_ary_02)
 end
-
 
 rename_project
